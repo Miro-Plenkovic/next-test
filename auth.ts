d52b74f0e5f8a39c325a972c9480a9734a4d9b41
miro plenkovic <miroplenkovic78@gmail.com>
2026-01-13 12:20:57 +0100

    middleware attempted fix

--- auth.ts
+++ auth.ts
@@ -1,37 +1,35 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
-import Credentials from 'next-auth/providers/credentials';
-import z from 'zod';
-import { sql } from '@vercel/postgres';
-import bcrypt from 'bcrypt';
-import type { User } from '@/app/lib/definitions';
- 
+// auth.ts
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { authConfig } from "./auth.config"
+import z from "zod"
+import { sql } from "@vercel/postgres"
+import bcrypt from "bcrypt"
+import type { User } from "@/app/lib/definitions"
+
 async function getUser(email: string): Promise<User | undefined> {
-    try {
-      const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-      return user.rows[0];
-    } catch (error) {
-      console.error('Failed to fetch user:', error);
-      throw new Error('Failed to fetch user.');
-    }
-  }
+  const user = await sql<User>`SELECT * FROM users WHERE email=${email}`
+  return user.rows[0]
+}
 
 export const { auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  providers: [Credentials({
-    async authorize(credentials) {
-      const parsedCredentials = z
-        .object({ email: z.string().email(), password: z.string().min(6) })
-        .safeParse(credentials);
-        if (parsedCredentials.success) {
-            const { email, password } = parsedCredentials.data;
-            const user = await getUser(email);
-            if (!user) return null;
-            const passwordsMatch = await bcrypt.compare(password, user.password);
- 
-            if (passwordsMatch) return user;
-        }
-        console.log('Invalid credentials');
-        return null;
-  }})],
-});
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsed = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials)
+
+        if (!parsed.success) return null
+
+        const { email, password } = parsed.data
+        const user = await getUser(email)
+        if (!user) return null
+
+        const ok = await bcrypt.compare(password, user.password)
+        return ok ? user : null
+      },
+    }),
+  ],
+})
