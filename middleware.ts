0c75cca6b25d271264ba4b9bde3df22e48c8459f
miro plenkovic <miroplenkovic78@gmail.com>
2026-01-13 14:32:27 +0100

    wrapping middleware for logging

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,16 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
  
-export default NextAuth(authConfig).auth;
+const authMiddleware = NextAuth(authConfig).auth
+
+export default async function middleware(req: any) {
+  try {
+    return await authMiddleware(req)
+  } catch (e) {
+    console.error("Middleware error:", e)
+    throw e
+  }
+}
  
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login).*)"],
