fb047521c6d7c40b5b4cb42320f5dc8a341be658
miro plenkovic <miroplenkovic78@gmail.com>
2026-01-13 14:01:36 +0100

    sigh

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,14 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+// import NextAuth from 'next-auth';
+// import { authConfig } from './auth.config';
  
-export default NextAuth(authConfig).auth;
+// export default NextAuth(authConfig).auth;
  
-export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  //matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login).*)"],
-};
+// export const config = {
+//   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+//   //matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login).*)"],
+// };
+
+export function middleware() {
+  return
+}
